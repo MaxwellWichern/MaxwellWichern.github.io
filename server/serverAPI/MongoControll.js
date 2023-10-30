12f4de66eb5ea8a243800e1c7cfa95d11ad2e153
Matthew Thomas <thomasm6883@my.uwstout.edu>
2023-10-29 21:46:43 -0500

    Maybe Fixed the Closed Topology by creating a new function replacin 'queryDatabase'

--- server/serverAPI/MongoControll.js
+++ server/serverAPI/MongoControll.js
@@ -6,16 +6,24 @@ Dotenv.config()
 const DB_USER = process.env.DB_USER ?? 'unknown'
 const DB_PASS = process.env.DB_PASS ?? 'unknown'
 
-//const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.voup7iz.mongodb.net/?retryWrites=true&w=majority`
-const uri = "mongodb+srv://Admin1:Admin123@cluster0.pkzerjw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.pkzerjw.mongodb.net/?retryWrites=true&w=majority`;
+
 
 
 const client = new Mongo.MongoClient(uri, {useNewUrlParser: true,useUnifiedTopology: true, serverApi: Mongo.ServerApiVersion.v1 })
 
-export default function queryDatabase(queryCallBack, databaseName) {
-  queryCallBack(client.db(databaseName))
-    .catch(err => {
-      console.error('---------NEW MONGO ERROR---------')
-      console.error(err)
-    })
+export default async function queryDatabase(queryCallBack, databaseName) {
+  let client;
+  try {
+    client = new Mongo.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: Mongo.ServerApiVersion.v1 });
+    await client.connect();
+    await queryCallBack(client.db(databaseName));
+  } catch (err) {
+    console.error('---------NEW MONGO ERROR---------');
+    console.error(err);
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
 }
