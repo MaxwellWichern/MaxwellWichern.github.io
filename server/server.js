cce55346cc676123b6b672b687d905a5515fa224
Matthew Thomas <thomasm6883@my.uwstout.edu>
2023-12-09 15:57:11 -0600

    small server side

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ app.get('*', (req, res) => {
 });
 
 // start listen
-app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 })
